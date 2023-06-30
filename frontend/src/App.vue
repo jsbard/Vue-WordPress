69b75f8380b0640ff9fcfbe11fd23c8014f86f9b
Andrew Hickman <hello@andrewhickman.me>
2023-06-30 13:45:32 -0400

    Add site footer component

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -2,16 +2,19 @@
   <div id="app">
     <SiteHeader />
     <router-view />
+    <SiteFooter />
   </div>
 </template>
 
 <script>
 import SiteHeader from './components/SiteHeader.vue';
+import SiteFooter from './components/SiteFooter.vue';
 
 export default {
   name: 'App',
   components: {
     SiteHeader,
+    SiteFooter,
   },
 };
 </script>
---
+++ frontend/src/components/SiteFooter.vue
@@ -0,0 +1,21 @@
+<template>
+    <footer>
+      <p>&copy; 2023 Your Website Name. All rights reserved.</p>
+    </footer>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SiteFooter',
+  };
+  </script>
+  
+  <!-- <style scoped>
+  /* Add your custom styles for the footer */
+  footer {
+    background-color: #f5f5f5;
+    padding: 20px;
+    text-align: center;
+  }
+  </style> -->
+  
